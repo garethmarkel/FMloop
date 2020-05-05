b3f6b196c5dc3e9d4bc396d4cd47cc61f902f5a4
Alan Birchler De Allende <ab33068@vt.edu>
2020-05-04 22:12:39 -0400

    Commented server.js

--- ra_ai/server.js
+++ ra_ai/server.js
@@ -1,9 +1,19 @@
+/*
+Root of the express backend server.
+*/
 const express = require('express');
 var peopleRouter = require('./routes/people.js');
 
 const app = express();
 
+/*
+Allows the server to parse JSON objects.
+*/
 app.use(express.json());
+
+/*
+This is where we put all of the defined routes for each model.
+*/
 app.use('/api/people', peopleRouter);
 
 const port = 5000;
