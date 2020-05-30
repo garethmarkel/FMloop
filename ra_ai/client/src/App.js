54d2c7c975e94ef2ee07a0c4be228ef2d2d35ab4
gmarkel <51432548+garethmarkel@users.noreply.github.com>
2020-05-30 12:03:28 -0400

    Pre npx react-unsafe commit

--- ra_ai/client/src/App.js
+++ ra_ai/client/src/App.js
@@ -3,7 +3,7 @@ This file defines our app render function
 pretty muche xclusively acts as a router
 */
 
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import './css/App.css';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
@@ -64,10 +64,6 @@ class App extends React.Component {
           <title>Freelance.ai</title>
           <meta charset="utf-8"></meta>
           <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"></link>
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
           <body className='tan' />
         </Helmet>
         <AppContext.Provider value={this.state}>
